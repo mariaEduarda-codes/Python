df32aa263977fad215aefc73884829294959a3f7
Maria Eduarda <78294169+mariaEduarda-codes@users.noreply.github.com>
2021-03-21 01:41:55 -0300

    Create condicional5.py

---
+++ estrutura-condicional/condicional5.py
@@ -0,0 +1,11 @@
+"""
+Faça um programa que receba um número inteiro e verifique
+se este número é par ou ímpar
+"""
+
+numero = int(input("Digite um número inteiro: "))
+
+if numero % 2 == 0:
+    print("Par")
+else:
+    print("Ímpar")
