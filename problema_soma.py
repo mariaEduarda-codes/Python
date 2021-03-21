976c784c7ba5816847adc69ad97acdf0d68e4237
Maria Eduarda <78294169+mariaEduarda-codes@users.noreply.github.com>
2021-03-21 01:26:37 -0300

    Delete problema_soma.py

--- problema_soma.py
+++
@@ -1,9 +0,0 @@
-"""
-Fazer um programa para ler dois valores inteiros
-X e Y, e depois mostrar na tela o valor da soma
-destes números.
-"""
-
-x = int(input("Digite o valor de X: "))
-y = int(input("Digite o valor de Y: "))
-print(f"SOMA = {x + y}")
