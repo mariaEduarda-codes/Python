69dfdd80826d51e4aa4ded0932a174c962c22139
Maria Eduarda <78294169+mariaEduarda-codes@users.noreply.github.com>
2021-03-21 01:34:34 -0300

    Create condicional1.py

---
+++ estrutura-condicional/condicional1.py
@@ -0,0 +1,13 @@
+"""
+Faça um programa que receba dois números e mostre qual
+deles é maior
+"""
+numero1 = int(input("Insira um número: "))
+numero2 = int(input("Insira outro número: "))
+
+print("O maior deles é: ")
+
+if numero1 > numero2:
+    print(numero1)
+else:
+    print(numero2)
